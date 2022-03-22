f934c69cda752824f701a4f390897706b6de4393
Porrapat Petchdamrongskul <porrapat.cpe20@gmail.com>
2022-03-22 15:51:50 +0700

    More Code

--- get_next_line.c
+++ get_next_line.c
@@ -13,9 +13,47 @@
 // norminette
 // gcc -Wall -Wextra -Werror get_next_line.c
 // gcc -Wall -Wextra -Werror get_next_line.c && ./a.out && rm -rf a.out
+// gcc get_next_line.c && ./a.out && rm -rf a.out
 #include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#define BUFFER_SIZE 10
 
 int	main(void)
 {
+	int fd;
+	int ret;
+	char *buff;
+
+	fd = 0;
+	fd = open("files/mypao", O_RDWR);
+	ret = 0;
+	if(fd == -1)
+	{
+
+	}
+	else
+	{
+		printf("%d\n", fd);
+		buff = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
+		ret = read(fd, buff, BUFFER_SIZE);
+		buff[BUFFER_SIZE] = '\0';
+
+		printf("ret is %d\n", ret);
+		printf("Buff is %s\n", buff);
+
+		ret = read(fd, buff, BUFFER_SIZE);
+		buff[BUFFER_SIZE] = '\0';
+
+		printf("ret is %d\n", ret);
+		printf("Buff is %s\n", buff);
+
+		free(buff);
+	}
+	printf("%d\n", fd);
+
+	// char *get_next_line(int fd);
 	return (0);
 }

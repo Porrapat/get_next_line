10f3a2921d8deb650f955d89a0715c8e101ff7cc
Porrapat Petchdamrongskul <porrapat.cpe20@gmail.com>
2022-03-22 15:34:04 +0700

    Add more main here

--- get_next_line.c
+++ get_next_line.c
@@ -9,3 +9,13 @@
 /*   Updated: 2022/03/13 20:18:35 by ppetchda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+// norminette
+// gcc -Wall -Wextra -Werror get_next_line.c
+// gcc -Wall -Wextra -Werror get_next_line.c && ./a.out && rm -rf a.out
+#include <stdio.h>
+
+int	main(void)
+{
+	return (0);
+}

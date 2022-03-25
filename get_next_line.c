71b4e1446feda5733257a4da7c5197504e05392c
Porrapat Petchdamrongskul <porrapat.cpe20@gmail.com>
2022-03-25 16:13:47 +0700

    Can Run But Has Lot of Memleak

--- get_next_line.c
+++ get_next_line.c
@@ -16,7 +16,6 @@
 char	*get_next_line(int fd)
 {
 	char		*buff;
-	int			return_bytes;
 	static char	*main_str;
 	int			old_len;
 	char		*new_str;
@@ -26,9 +25,7 @@ char	*get_next_line(int fd)
 	{
 		main_str = (char *)malloc(1);
 		main_str[0] = '\0';
-		// printf("Hey\n");
 	}
-	// printf("Hell\n");
 	buff = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
 
 	while(read(fd, buff, BUFFER_SIZE) > 0)
@@ -39,7 +36,7 @@ char	*get_next_line(int fd)
 		
 		main_str[old_len] = '\0';
 		ft_strlcat(main_str, buff, ft_strlen(buff) + ft_strlen(main_str) + 1);
-		if(new_str = ft_strnstr(main_str, "\n", ft_strlen(main_str)))
+		if((new_str = ft_strnstr(main_str, "\n", ft_strlen(main_str))))
 		{
 			to_return_str = malloc(new_str - main_str + 2);
 			ft_memcpy(to_return_str, main_str, new_str - main_str + 1);
@@ -71,7 +68,7 @@ int	main(void)
 	round = 1;
 	if(fd != -1)
 	{
-		while(result = get_next_line(fd))
+		while((result = get_next_line(fd)))
 		{
 			printf("%d. Line is %s*\n", round++, result);
 			free(result);
@@ -86,113 +83,3 @@ int	main(void)
 	close(fd);
 	return (0);
 }
-
-
-
-// 1. Read แล้วต่อ String เรื่อยๆ จนกว่าเจอ \n แล้ว Return ออกอันนึง จากนั้น Read ต่ออีก
-// 1.1 เราต้องมีฟังก์ชันที่ใช้อะไรบ้าง
-
-// ft_strchr ---> X ไม่ใช้
-// ft_substr
-// ft_strlcat ต่อสตริง เอาความยาวจากสตริง Len ทั้งคู่มาต่อกัน
-// ft_strjoin ต่อสตริง ---> x ไม่ใช้
-// char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
-
-
-// Story 1
-// int		fd;
-// char	*result;
-// int		round;
-
-// fd = 0;
-// fd = open("files/mypao", O_RDWR);
-
-// round = 1;
-// if(fd != -1)
-// {
-// 	while(result = get_next_line(fd))
-// 	{
-// 		printf("%d. Line is %s*", round++, result);
-// 		free(result);
-// 	}
-// }
-
-
-// // Story 2
-// int ret = 0;
-// const char *src = "def";
-// char *dst;
-
-// dst = (char *)malloc(4);
-// dst[0] = 'a';
-// dst[1] = 'b';
-// dst[2] = 'c';
-// dst[3] = '\0';
-
-// int old_len = ft_strlen(dst);
-// dst = ft_realloc(dst, ft_strlen(src) + ft_strlen(dst) + 1);
-// dst[old_len] = '\0';
-
-// ret = ft_strlcat(dst, src, ft_strlen(src) + ft_strlen(dst) + 1);
-// printf("dst is %s\n", dst);
-// printf("ret is %d\n", ret);
-// free(dst);
-
-// // Story 3
-// char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
-
-// const char *myhay = "abcde\ndef\nghijk";
-
-// // char *myhay;
-// // myhay = (char *)malloc(16);
-// // ft_memcpy(myhay, "abcde\ndef\nghijk", 16);
-
-// char *temp;
-
-// // printf("%s---", myhay);
-
-// temp = ft_strnstr(myhay, "\n", ft_strlen(myhay));
-// printf("+%s+", myhay);
-// printf("+%s+", temp+1);
-// // free(myhay);
-
-
-
-// return_bytes = 0;
-// buff = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
-// return_bytes = read(fd, buff, BUFFER_SIZE);
-// if (return_bytes == 0)
-// {
-// 	free(buff);
-// 	return (NULL);
-// }
-// buff[return_bytes] = '\0';
-// main_str = (char *)malloc(1);
-// main_str[0] = '\0';
-
-// while(ft_strnstr(buff, "\n", ft_strlen(buff) == NULL))
-// {
-// 	old_len = ft_strlen(main_str);
-// 	main_str = ft_realloc(main_str, ft_strlen(buff) + ft_strlen(main_str) + 1);
-// 	main_str[old_len] = '\0';
-	
-// 	ft_strlcat(main_str, buff, ft_strlen(buff) + ft_strlen(main_str) + 1);
-
-// free(main_str);
-
-// free(buff);
-// while(ft_strnstr(buff, "\n", ft_strlen(buff) == NULL))
-// {
-// 	printf("No Find Newline\n");
-// 	buff = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
-// 	return_bytes = read(fd, buff, BUFFER_SIZE);
-// 	if(return_bytes == 0)
-// 	{
-// 		free(buff);
-// 		return (NULL);
-// 	}
-// 	buff[return_bytes] = '\0';
-// }
-
-
-// return (buff);

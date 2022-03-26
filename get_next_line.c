1920d8324946c9956508212c7e510595f8420ae8
Porrapat Petchdamrongskul <porrapat.cpe20@gmail.com>
2022-03-26 16:06:22 +0700

    Doing Here

--- get_next_line.c
+++ get_next_line.c
@@ -11,20 +11,30 @@
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <stdio.h>
 
 static char	*make_result_str(int fd)
 {
-	static char	*main_str = "Pao\nIs\nSmart\n";
+	static char	main_str[] = "1";
 	char *result_str;
+	char *pao_str = "23";
 
 	result_str = malloc(ft_strlen(main_str) + 1);
 	ft_memmove(result_str, main_str, ft_strlen(main_str) + 1);
 	if (fd == -1)
 		return (NULL);
+
+	// // main_str = pao_str;
+	// ft_strlen(main_str);
+	// ft_strlen(pao_str);
+	ft_strlcat(main_str, pao_str, ft_strlen(main_str) + ft_strlen(pao_str) + 1);
 	
-	free(result_str);
-	return (NULL);
-	// return (result_str);
+	// printf("----- In make_result_str -----\n");
+	// printf("main_str = %s\n", main_str);
+	
+	// free(result_str);
+	// return (NULL);
+	return (result_str);
 }
 
 static char	*get_next_line_main_logic(int fd)
